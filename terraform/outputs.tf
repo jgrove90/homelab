4bef0d433ef711bf8c7efa23ae60242ff1c8194c
jgrove90 <jgrove90@gmail.com>
2025-04-03 21:52:26 -0600

    add worker config outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,12 +8,25 @@ output "kubeconfig" {
   sensitive = true
 }
 
-output "client_configuration" {
-  value = yamlencode(talos_machine_secrets.machine_secrets.client_configuration)
-  sensitive = true
-}
-
 output "machineconfig_control_plane" {
   value     = yamlencode(data.talos_machine_configuration.machineconfig_control_plane)
   sensitive = true
+}
+
+output "machineconfig_worker_01" {
+  value     = yamlencode(data.talos_machine_configuration.machineconfig_worker_01)
+  sensitive = true
+}
+
+output "machineconfig_worker_02" {
+  value     = yamlencode(data.talos_machine_configuration.machineconfig_worker_02)
+  sensitive = true
+}
+
+output "control_plane_ip" {
+  value = var.talos_control_plane_ip_addr
+}
+
+output "worker_ips" {
+  value = [var.talos_worker_01_ip_addr, var.talos_worker_02_ip_addr]
 }
